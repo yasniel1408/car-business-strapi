3bd558db0cb6bb631622cd267bf568d5b5c87f2b
Yasniel PC <yasnielfajardoegues1408@gamil.com>
2021-06-21 11:03:27 -0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e8bea278088cfe82de4c16931843d419'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '74f41a0d-611b-4d9f-81f5-e589f484a8d7'
+};
